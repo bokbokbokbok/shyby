f9ee6a0e8bae33f4579beef28adec0a0f948763f
bokbokbokbok <57594308+bokbokbokbok@users.noreply.github.com>
2020-12-21 14:28:37 +0200

    йцу

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -2,63 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
 import { FormGroup } from '@angular/forms';
 
+interface sessionUser {
+  name: string,
+  id: number
+}
+
 @Component({
   selector: 'app-loginpage',
   templateUrl: './loginpage.component.html',
   styleUrls: ['./loginpage.component.css']
 })
+
 export class LoginpageComponent implements OnInit {
   ngOnInit(): void {
   }
+  sessionType?: number;
+  sessionUser?: sessionUser;
+  sessionTable?: number;
 
-  form = new FormGroup({});
-  model: any = {};
-  options: FormlyFormOptions = {};
-  users =["Tom", "Bob", "Sam", "Bill"];
+  setType(num: number){
+    this.sessionType = num;
+  }
+  setUser(su: sessionUser){
+    this.sessionUser= su;
+  }
+
+  users: sessionUser[] =[{"name": "Tom", id: 1},
+   {"name": "Bob", id: 2},
+    {"name": "Sam", id: 3},
+     {"name": "Bill", id: 4}
+    ];
   dropdownUsers = false;
   dropdownNumbers = false;
-  
-  fields: FormlyFieldConfig[] = [
-    {
-      type: 'button',
-      templateOptions: {
-        text: 'Wybierz stolik',
-        btnType: 'white',
-        onClick: ($event) => {
-          this.form.get('someInput').setValue('clicked!');
-        },
-      },
-    },
-    {
-      type: 'btngroup',
-      templateOptions: {
-        text: 'Stolik',
-        btnAlignment: 'horizontal',
-        btnType: 'white',
-        onClick: ($event) => {
-          this.form.get('someInput').setValue('clicked!');
-        },
-      },
-    },
-    {
-      type: 'button',
-      templateOptions: {
-        text: 'Wybierz pracownika',
-        btnType: 'white',
-        onClick: ($event) => {
-          this.form.get('someInput').setValue('clicked!');
-        },
-      },
-    },
-    {
-      key: 'iLikeTwix',
-      type: 'checkbox',
-      templateOptions: {
-        label: 'I like twix',
-      },
-      hideExpression: '!model.name',
-    },
-  ];
 
   toggledropdownNumbers() {
     this.dropdownUsers = false;
@@ -72,26 +47,42 @@ export class LoginpageComponent implements OnInit {
 
   calculator = {
     displayValue: 'Wybierz stolik',
-    waitingForSecondOperand: false,
+    waitingForSecondOperand: false
   };
 
-  inputDigit(digit) {
+  inputDigit(digit: any) {
     const { displayValue, waitingForSecondOperand } = this.calculator;
   
     if (waitingForSecondOperand === true) {
       this.calculator.displayValue = digit;
       this.calculator.waitingForSecondOperand = false;
     } else {
-      this.calculator.displayValue = displayValue === 'Wybierz stolik' ||  displayValue === '0' ? '' + digit : '' + displayValue + digit;
+     if (displayValue === 'Wybierz stolik' ||  displayValue === '0')
+     {
+      this.calculator.displayValue ='' + digit;
+      this.sessionTable = digit;
+     }
+     else
+     {
+      var toadd = displayValue + digit;
+      this.calculator.displayValue = '' + toadd;
+      this.sessionTable = +toadd;
+     }
     }
   }
 
   resetCalculator() {
     this.calculator.displayValue = 'Wybierz stolik';
+    this.sessionTable = undefined;
     this.calculator.waitingForSecondOperand = false;
   }
   
   submit() {
-    alert(JSON.stringify(this.model));
+    // let session: DmSession = {
+    //   workerId: this.sessionUser,
+    //   assemblyTableId: this.sessionType,
+    //   assemblyTableOperationModeId: this.sessionTable
+    // };
+    // alert(DmSessionToJSON(session));
   }
 }
